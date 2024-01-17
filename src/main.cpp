748d3c6e4ec111285ae011e61f49b44cf3037823
AD-laD <81196308+AD-laD@users.noreply.github.com>
2024-01-17 17:58:57 +0100

    premier gui

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,27 @@ int main()
     // Actual application code
     auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
     ctx.maximize_window();
+    auto        square_radius = 0.5f;
+    std::string text          = "Hello";
+    ctx.imgui                 = [&]() {
+        // Show a simple window
+        ImGui::Begin("Test");
+        ImGui::SliderFloat("Square size", &square_radius, 0.f, 1.f);
+        ImGui::InputText("Text", &text);
+        ImGui::End();
+        // Show the official ImGui demo window
+        // It is very useful to discover all the widgets available in ImGui
+        ImGui::ShowDemoWindow();
+    };
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
         ctx.background(p6::NamedColor::Blue);
-        ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.2f}
-        );
+        // ctx.circle(
+        //     p6::Center{ctx.mouse()},
+        //     p6::Radius{0.2f}
+        // );
+        ctx.square(p6::Center{}, p6::Radius{square_radius});
     };
 
     // Should be done last. It starts the infinite loop.
