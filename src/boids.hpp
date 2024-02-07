a5772c72c9adce82f07733f70f96de2111574391
AD-laD <81196308+AD-laD@users.noreply.github.com>
2024-02-07 14:47:54 +0100

    class boids suite

--- src/boids.hpp
+++ src/boids.hpp
@@ -12,11 +12,13 @@
 class Boids {
 private:
     std::vector<Boid> m_boids; // vecteur de Boid
-    int               m_numberOfBoids;
+    // int               m_numberOfBoids;
 
 public:
     Boids(std::vector<Boid> vec, int number);
     ~Boids() = default;
+    std::vector<Boid>   getVect() const; // get le vector de Boids
+    int                 NumberOfBoids() const;
     Boid                getBoid(int id);
     void                addBoid(const Boid& boid);
     void                draw(p6::Context& ctx); // dessine tous les Boids
@@ -28,6 +30,22 @@ public:
 Boids::Boids(std::vector<Boid> vec, int number)
     : m_boids(vec), m_numberOfBoids(number) {}
 
+std::vector<Boid> Boids::getVect() const
+{ // getter du vecteur de boids
+    std::vector<Boid> boids = m_boids;
+    return (boids);
+}
+
+int Boids::NumberOfBoids() const
+{
+    return (m_boids.size());
+}
+
 void Boids::draw(p6::Context& ctx)
 {
+}
+
+void Boids::addBoid(const Boid& boid)
+{
+    getVect().push_back(std::move(boid));
 }
