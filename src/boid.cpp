f9fde2fe1a2e2983cfeac470bad2dd70a85ec937
lila <lilacappelie@gmail.com>
2024-02-07 15:00:42 +0100

    lila

--- src/boid.cpp
+++ src/boid.cpp
@@ -1,4 +1,30 @@
+#pragma once
+
 #include "boid.hpp"
 
 static constexpr float maxSpeed = 5;
 static constexpr float maxForce = 10;
+
+Boid::Boid()
+    : m_position(glm::vec3{0, 0, 0}), m_speed(glm::vec3{0.001, 0.001, 0}) {}
+
+glm::vec3 Boid::get_position() const
+{
+    return m_position;
+}
+glm::vec3 Boid::get_speed() const
+{
+    return m_speed;
+}
+
+void Boid::draw(p6::Context& ctx)
+{
+    ctx.fill = {1, 1, 1, 0.5};
+    ctx.square(p6::Center{get_position()}, p6::Radius{0.05f});
+}
+
+void Boid::move()
+{
+    glm::vec3 new_pos = get_speed() + get_position();
+    m_position        = new_pos;
+}
--- src/boid.hpp
+++ src/boid.hpp
@@ -20,26 +20,3 @@ public:
     void      draw(p6::Context& ctx);
 };
 
-Boid::Boid()
-    : m_position(glm::vec3{0, 0, 0}), m_speed(glm::vec3{0.001, 0.001, 0}) {}
-
-glm::vec3 Boid::get_position() const
-{
-    return m_position;
-}
-glm::vec3 Boid::get_speed() const
-{
-    return m_speed;
-}
-
-void Boid::draw(p6::Context& ctx)
-{
-    ctx.fill = {1, 1, 1, 0.5};
-    ctx.square(p6::Center{get_position()}, p6::Radius{0.05f});
-}
-
-void Boid::move()
-{
-    glm::vec3 new_pos = get_speed() + get_position();
-    m_position        = new_pos;
-}
