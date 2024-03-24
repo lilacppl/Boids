b8e81cc6cbe80e5659cb5eb1ce690027de9d7922
lila <lilacappelie@gmail.com>
2024-03-24 16:11:12 +0100

    lila sphere

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include "../glimac/common.hpp"
 #include "../glimac/sphere_vertices.hpp"
+#include "boid3d.hpp"
 #include "p6/p6.h"
 #include "scene.hpp"
 #include "vao.hpp"
@@ -36,41 +37,29 @@ int main()
 
     const std::vector<glimac::ShapeVertex> vertices = glimac::sphere_vertices(1.f, 32, 16); // création des vertices de la sphere
 
-    // VBO
     VBO vbo(vertices);
     VAO vao;
     vao.bind();
-    static constexpr GLuint pos1 = 0;
-    static constexpr GLuint pos2 = 1;
-    static constexpr GLuint pos3 = 2;
-
     vbo.bind();
-    // vao.vertex_attrib(&pos, &norm, &color);
-    glEnableVertexAttribArray(pos1);
-    glEnableVertexAttribArray(pos2);
-    glEnableVertexAttribArray(pos3);
-
-    // on spécifie les attributs du vertex
-    glVertexAttribPointer(pos1, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), nullptr);
-    glVertexAttribPointer(pos2, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)(offsetof(glimac::ShapeVertex, normal)));
-    glVertexAttribPointer(pos3, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)(offsetof(glimac::ShapeVertex, texCoords)));
+    vao.vertex_attrib();
     vbo.debind();
     vao.debind();
 
-    Scene scene;
-    scene.InitializeClasses();
+    // Scene scene;
+    // scene.InitializeClasses();
     std::string text = "Hello";
     ctx.imgui        = [&]() {
     };
-    vao.bind();
     // Declare your infinite update loop.
     ctx.update = [&]() {
+        vao.bind();
+        shader.use();
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST);
-        glDrawArrays(GL_TRIANGLES, 0, vertices.size());
-        scene.update(ctx);
+        draw_sphere(&shader, vertices, &ctx);
+        // scene.update(ctx)
+        vao.debind();
     };
-    vbo.debind();
     // Should be done last. It starts the infinite loop.
     ctx.start();
 }
