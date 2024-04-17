c8375c146305e14b24cb6a59345652f93d1bb68e
AD-laD <81196308+AD-laD@users.noreply.github.com>
2024-04-17 19:11:23 +0200

    class ObjectPosition et struct objects vector pour lister les position et scale des éléments de la scène

--- src/scene.hpp
+++ src/scene.hpp
@@ -10,8 +10,8 @@
 #include "imguivariables.hpp"
 #include "iprogram.hpp"
 #include "mesh.hpp"
+#include "objectsvector.hpp"
 #include "vertices.hpp"
-
 
 class Scene {
 private:
@@ -21,15 +21,21 @@ private:
     Mesh           m_cube;
     Mesh           m_fish;
     // FreeflyCamera  m_camera;
-    Program           m_fish_program;
-    Program           m_cube_program;
-    Arpenteur         m_arpenteur;
+    Program m_fish_program;
+    Program m_cube_program;
+    Program m_arpenteur_program;
+    Program m_seaweed_program;
+    // std::vector<Program> programs ; //a faire
+    Arpenteur m_arpenteur;
     // std::vector<Mesh> m_seaweed;
-    Mesh m_seaweed;
+    Mesh              m_seaweed;
+    SceneObjectVector m_objects;
+    glm::mat4         m_viewMatrix;
 
 public:
     // Scene(p6::Context& ctx);
     Scene();
     void Init(p6::Context& ctx);
     void update(p6::Context& ctx);
+    void draw(p6::Context& ctx);
 };
