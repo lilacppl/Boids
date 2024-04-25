2dda88023c04a3dd28b5967fde3422a8d0645a5a
AD-laD <81196308+AD-laD@users.noreply.github.com>
2024-04-25 09:50:43 +0200

    retour ancienne cam

--- src/arpenteur.cpp
+++ src/arpenteur.cpp
@@ -1,2 +1,143 @@
 #pragma once
 #include "arpenteur.hpp"
+
+Arpenteur::Arpenteur()
+    : m_position(glm::vec3(0.0f)), m_mesh(m_vertices.fish), m_Phi(0.), m_Theta(0.0f)
+{
+    m_camera.updatePosition(m_position, 0);
+}
+
+glm::vec3 Arpenteur::getArpenteurPosition()
+{
+    return m_position;
+}
+
+void Arpenteur::update(const p6::Context& ctx, const Program& program, const float height, const int& time)
+{
+    m_camera.updatePosition(m_position, m_Phi);
+    handleMapBounds(4.0, height);
+    m_mesh.DrawMesh(ctx, m_camera.getViewMatrix(m_position), program, m_position, 0.1, glm::vec3(0., 0., 0.), 1., time);
+}
+
+glm::mat4 Arpenteur::getViewMatrix()
+{
+    return m_camera.getViewMatrix(m_position);
+}
+
+void Arpenteur::moveLeft(float t)
+{
+    m_position += t * m_LeftVector;
+}
+
+void Arpenteur::moveFront(float t)
+{
+    m_position += t * m_FrontVector;
+}
+
+void Arpenteur::moveUp(float t)
+{
+    // Calculer le vecteur de déplacement en fonction de l'axe Up
+    glm::vec3 upMovement = t * m_UpVector;
+    // Ajouter le déplacement à la position de la caméra
+    m_position += upMovement;
+}
+
+// Méthode pour tourner la caméra autour de l'arpenteur
+void Arpenteur::rotateLeft(float degrees)
+{
+    m_Phi += glm::radians(degrees);
+}
+
+void Arpenteur::handleMapBounds(const float square_radius, float height)
+{
+    float margin = 1.0;
+    if (m_position[0] < -square_radius)
+    {
+        m_position.x = square_radius - 0.1;
+    }
+    if (m_position[0] > square_radius * margin)
+    {
+        // m_position.x *= -1;
+        m_position.x = -square_radius + 0.1;
+    }
+    if (m_position[1] < -square_radius * margin)
+    {
+        m_position.y = square_radius - 0.1;
+        // m_position.y *= -1;
+    }
+    // if (m_position[1] > height * margin)
+    if (m_position[1] > square_radius * margin)
+    {
+        m_position.y = -square_radius + 0.1;
+    }
+    if (m_position[2] < -square_radius * margin)
+    {
+        m_position.z = square_radius - 0.1;
+    }
+    if (m_position[2] > square_radius * margin)
+    {
+        m_position.z = -square_radius + 0.1;
+    }
+}
+
+void Arpenteur::eventManager(p6::Context& ctx)
+{
+    ctx.key_pressed = [&](p6::Key key) {
+        if (key.logical == "z")
+            m_moveForward = true;
+        else if (key.logical == "s")
+            m_moveBackward = true;
+        else if (key.logical == "q")
+            m_moveLeft = true;
+        else if (key.logical == "d")
+            m_moveRight = true;
+        else if (key.logical == " ")
+            m_moveUp = true;
+        else if (key.logical == "w")
+            m_moveDown = true;
+        else if (key.logical == "a")
+            m_rotateLeft = true;
+        else if (key.logical == "e")
+            m_rotateRight = true;
+    };
+
+    // Gestionnaire d'événements de touche relâchée
+    ctx.key_released = [&](p6::Key key) {
+        if (key.logical == "z")
+            m_moveForward = false;
+        else if (key.logical == "s")
+            m_moveBackward = false;
+        else if (key.logical == "q")
+            m_moveLeft = false;
+        else if (key.logical == "d")
+            m_moveRight = false;
+        else if (key.logical == " ")
+            m_moveUp = false;
+        else if (key.logical == "w")
+            m_moveDown = false;
+        else if (key.logical == "a")
+            m_rotateLeft = false;
+        else if (key.logical == "e")
+            m_rotateRight = false;
+    };
+}
+
+void Arpenteur::eventUpdate()
+{
+    if (m_moveForward)
+        moveFront(m_speed);
+    if (m_moveBackward)
+        moveFront(-m_speed);
+    if (m_moveLeft)
+        moveLeft(m_speed);
+    if (m_moveRight)
+        moveLeft(-m_speed);
+    if (m_moveUp)
+        moveUp(m_speed);
+    if (m_moveDown)
+        moveUp(-m_speed);
+    if (m_rotateLeft)
+        rotateLeft(10 * m_sensi);
+    if (m_rotateRight)
+        rotateLeft(-10 * m_sensi);
+}
--- src/arpenteurcamera.hpp
+++ src/arpenteurcamera.hpp
@@ -28,36 +28,12 @@ public:
         m_LeftVector  = glm::vec3(0, 0, 1);
         m_UpVector    = glm::vec3(0, 1, 0);
     }
-    void computeDirectionVectors()
-    {
-        // float cosPhi = cos(m_Phi);
-        // float sinPhi = sin(m_Phi);
-        // // m_FrontVector = glm::vec3(-1.0 * (cosPhi), 0.0, 1.0 * sinPhi);
-        // m_FrontVector = glm::vec3(1.0 * (cosPhi), 0.0, 1.0 * sinPhi);
-        // m_FrontVector = glm::normalize(m_FrontVector);
-        // m_LeftVector  = glm::cross(m_UpVector, m_FrontVector);
-        // // m_LeftVector  = glm::vec3(glm::cos(m_Phi + (p6::PI / 2)), 0, glm::sin(m_Phi + (p6::PI / 2)));
-        // m_LeftVector = glm::normalize(m_LeftVector);
-
-        // float cosPhi   = cos(m_Phi);
-        // float sinPhi   = sin(m_Phi);
-        // float cosTheta = cos(m_Theta);
-        // float sinTheta = sin(m_Theta);
-
-        // m_FrontVector = glm::vec3(cosTheta * sinPhi, sinTheta, cosTheta * cosPhi);
-        // m_LeftVector  = glm::vec3(sin(m_Phi + glm::pi<float>() / 2), 0, cos(m_Phi + glm::pi<float>() / 2));
-        // m_UpVector    = glm::cross(m_FrontVector, m_LeftVector);
-    }
 
     void updatePosition(const glm::vec3 arpenteurPos, const float arpenteurRotation)
     {
         glm::vec4 positionTemp = glm::vec4(arpenteurPos.x, arpenteurPos.y, arpenteurPos.z, 0);
         positionTemp += (glm::rotate(glm::mat4(1), (p6::degrees_to_radians(arpenteurRotation)).value, glm::vec3(0, 1, 0)) * glm::vec4(m_distance, m_distance / 2, 0, 0));
-        // positionTemp += (glm::rotate(glm::mat4(1), (p6::degrees_to_radians(0)).value, glm::vec3(0, 1, 0)) * glm::vec4(m_distance, m_distance / 2, 0, 0));
         m_Position = glm::vec3(positionTemp.x, positionTemp.y, positionTemp.z);
-        // m_Phi      = arpenteurRotation;
-        // computeDirectionVectors();
-        // std::cout << "Position: (" << m_Position.x << ", " << m_Position.y << ", " << m_Position.z << ")" << std::endl;
     }
 
     // Méthode pour calculer et renvoyer la matrice de vue associée à la caméra
@@ -66,35 +42,7 @@ public:
         glm::vec3 eye    = m_Position;        // position de la camera
         glm::vec3 target = arpenteurPosition; // point qu'elle regarde
         glm::vec3 up     = m_UpVector;        // vecteur vertical
-
-        // return glm::lookAt(eye, target, up);
-
-        // // Vecteur vers la gauche (left) en utilisant le produit vectoriel entre le vecteur de direction et le vecteur up
-        // glm::vec3 front = glm::normalize(m_Position - arpenteurPosition);
-        // glm::vec3 left  = glm::normalize(glm::cross(up, front));
-
-        // // Vecteur vers le haut (up) en utilisant à nouveau le produit vectoriel entre left et front
-        // glm::vec3 newUp = glm::cross(front, left); // Cela assure que les vecteurs sont perpendiculaires et forment un système orthogonal
-
-        // // Mettre à jour les vecteurs de la caméra
-        // m_FrontVector += front;
-        // m_LeftVector += left;
-        // m_UpVector = newUp;
-
         return glm::lookAt(eye, target, up);
     }
 
-    glm::vec3 getFrontVector()
-    {
-        return m_FrontVector;
-    }
-    glm::vec3 getLeftVector()
-    {
-        return m_LeftVector;
-    }
-
-    glm::vec3 getUpVector()
-    {
-        return m_UpVector;
-    }
 };
